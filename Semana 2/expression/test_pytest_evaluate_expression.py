1ffef1d7b7f2aa61a4c0122c8575bb3d92a38f48
Juan Dausa <juandausa@gmail.com>
2020-03-18 16:31:36 -0300

    Se modifican casos de prueba para que sean mas complejos

--- Semana 2/expression/test_pytest_evaluate_expression.py
+++ Semana 2/expression/test_pytest_evaluate_expression.py
@@ -17,13 +17,13 @@ def test_statement_two_with_trailspaces():
 
 
 def test_sum_several_integers():
-    expresion = "+".join([str(x) for x in range(2001)])
-    assert evaluate_expression(expresion) == int(2000/2*(2000+1))
+    expresion = "+".join([str(x) for x in range(200001)])
+    assert evaluate_expression(expresion) == int(200000/2*(200000+1))
 
 
 def test_substract_several_integers():
-    expresion = "-".join([str(x) for x in range(2001)])
-    assert evaluate_expression(expresion) == -2000/2*(2000+1)
+    expresion = "-".join([str(x) for x in range(200001)])
+    assert evaluate_expression(expresion) == -200000/2*(200000+1)
 
 def test_complex_expression():
     expresion = "1+2+3+4-5-6-6+3+1+4+7-8-8-9-9+6+5-1+2+4-7-4-5+2+1-1-2+3+12-100"
