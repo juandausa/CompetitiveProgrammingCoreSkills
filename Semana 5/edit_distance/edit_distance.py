b354a4dd2522f6421b48121dee3e5edecc604e9b
Juan Dausa <juandausa@gmail.com>
2020-03-25 18:36:38 -0300

    Se agregan costos de operaciones a edit distance

--- Semana 5/edit_distance/edit_distance.py
+++ Semana 5/edit_distance/edit_distance.py
@@ -1,20 +1,19 @@
-def edit_distance(a, b, add_cost=1, remove_cost=1, edit_cost=1):
-    T = initialize(a, b)
-
+def edit_distance(a, b, add_cost=1, remove_cost=1, replace_cost=1):
+    T = initialize(a, b, add_cost, remove_cost)
     for i in range(1, len(a) + 1):
         for j in range(1, len(b) + 1):
-            diff = 0 if a[i - 1] == b[j - 1] else 1
-            T[i][j] = min(T[i - 1][j] + 1, T[i][j - 1] +
-                          1, T[i - 1][j - 1] + diff)
+            diff = 0 if a[i - 1] == b[j - 1] else replace_cost
+            T[i][j] = min(T[i - 1][j] + remove_cost, T[i][j - 1] +
+                          add_cost, T[i - 1][j - 1] + diff)
 
     return T[len(a)][len(b)]
 
 
-def initialize(a, b):
+def initialize(a, b, add_cost, remove_cost):
     T = [[float("inf")] * (len(b) + 1) for _ in range(len(a) + 1)]
     for i in range(len(a) + 1):
-        T[i][0] = i
+        T[i][0] = remove_cost * i
     for j in range(len(b) + 1):
-        T[0][j] = j
+        T[0][j] = add_cost * j
 
     return T
--- Semana 5/edit_distance/test_pytest_edit_distance.py
+++ Semana 5/edit_distance/test_pytest_edit_distance.py
@@ -12,3 +12,26 @@ def test_digit_redit():
 def test_information_applications():
     assert edit_distance("information", "applications") == 7
 
+
+def test_editing_distance_1_1_100():
+    assert edit_distance("editing", "distance", 1, 1, 100) == 7
+
+
+def test_editing_distance_100_1_1():
+    assert edit_distance("editing", "distance", 100, 1, 1) == 105
+
+
+def test_editing_distance_1_100_1():
+    assert edit_distance("editing", "distance", 1, 100, 1) == 6
+
+
+def test_a_b_100_1_1():
+    assert edit_distance("a", "b", 100, 1, 2) == 2
+
+
+def test_a_bb_100_1_1():
+    assert edit_distance("a", "bb", 100, 1, 1) == 101
+
+
+def test_bc_bbcc_100_1_1():
+    assert edit_distance("bc", "bbcc", 100, 1, 1) == 200
